cf3352a3ced6643de702b154ca23d5df17eb60aa
GBT <gbt3101@gmail.com>
2019-07-05 10:15:22 +0300

    Stalk form css and fun.

--- src/client/stalk-form/stalk-form.tsx
+++ src/client/stalk-form/stalk-form.tsx
@@ -1,14 +1,17 @@
 import * as React from 'react';
 import { Link } from 'react-router-dom';
+const css = require('./stalk-form.css');
 
 export class StalkForm extends React.Component<any> {
   public render() {
     return (
-      <form>
-        <div>Which user would you like to stalk?</div>
-        <input type='text' name='username' placeholder='User Name' required />
-        <button type='submit'>Stalk</button>
-      </form>
+      <div className={css.root}>
+          <h2 className={css.title}>Which user would you like to stalk?</h2>
+        <form className={css.formStyle}>
+          <input className={css.fieldStyle} type='text' name='username' placeholder='User Name' required />
+          <button className={`${css.pushButton} ${css.blue}`} type='submit'>Stalk</button>
+        </form>
+      </div>
     );
   }
 }
